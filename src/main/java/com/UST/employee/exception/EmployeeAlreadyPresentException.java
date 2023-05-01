ac83655fea7274b101807014ff4be7bccaa905b0
sona sudhakaran <sonasudhakaran12@gmail.com>
2023-05-01 14:55:16 +0530

    exception

---
+++ src/main/java/com/UST/employee/exception/EmployeeAlreadyPresentException.java
@@ -0,0 +1,9 @@
+package com.UST.employee.exception;
+
+public class EmployeeAlreadyPresentException extends RuntimeException{
+
+    public EmployeeAlreadyPresentException(String s) {
+
+        super(s);
+    }
+}
